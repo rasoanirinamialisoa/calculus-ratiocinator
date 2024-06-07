f030cf1c3c065ad233f14231da8835eac4b9411f
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-06-07 15:32:07 +0300

    feat: add class Verite|Mensonge

---
+++ src/main/java/com/example/calculusratiocinator/veriteEtMensonge/Mensonge.java
@@ -0,0 +1,9 @@
+package com.example.calculusratiocinator.veriteEtMensonge;
+import com.example.calculusratiocinator.Affirmation;
+
+public class Mensonge extends Affirmation {
+    @Override
+    public boolean estVraieOuFausse() {
+        return false;
+    }
+}
---
+++ src/main/java/com/example/calculusratiocinator/veriteEtMensonge/Verite.java
@@ -0,0 +1,8 @@
+package com.example.calculusratiocinator.veriteEtMensonge;
+import com.example.calculusratiocinator.Affirmation;
+public class Verite extends Affirmation {
+    @Override
+    public boolean estVraieOuFausse() {
+        return true;
+    }
+}
