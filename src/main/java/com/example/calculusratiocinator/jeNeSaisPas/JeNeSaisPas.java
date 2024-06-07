0d0f32dd46978b6abfe1dd49471ac206ce92bdcf
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-06-07 16:01:02 +0300

    feat: add class JeNeSaisPas

---
+++ src/main/java/com/example/calculusratiocinator/jeNeSaisPas/JeNeSaisPas.java
@@ -0,0 +1,14 @@
+package com.example.calculusratiocinator.jeNeSaisPas;
+import com.example.calculusratiocinator.Affirmation;
+
+public class JeNeSaisPas extends Affirmation {
+
+    @Override
+    public boolean estVraieOuFausse() {
+        return false;
+    }
+    public String obtenirReponse() {
+        return "Je ne sais pas";
+    }
+}
+
