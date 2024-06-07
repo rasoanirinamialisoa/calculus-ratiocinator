068c0deffb35fc8cfa2573b79fcf268f41580b93
rasoanirinamialisoa <hei.lisa.30@gmail.com>
2024-06-07 13:37:44 +0300

    first commit

---
+++ src/main/java/com/example/calculusratiocinator/CalculusRatiocinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculusratiocinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculusRatiocinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculusRatiocinatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculusratiocinator/CalculusRatiocinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculusratiocinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculusRatiocinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
